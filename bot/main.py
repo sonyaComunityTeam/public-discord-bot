e00614d0dd8ba2b4cf059e4608138e69bf9c92eb
NEPTARUNE <98255890+NEPTARUNE@users.noreply.github.com>
2022-10-22 12:40:32 +0900

    更新:main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,6 @@
 import discord
+import load_env
 
-TOKEN = TOKEN HERE
 bot = discord.Client(intents=discord.Intents.all())
 
 @bot.event
@@ -11,4 +11,4 @@ async def on_ready():
     print('------')
     await bot.change_presence(activity=discord.Game(name="そにゃコミュニティPublic Bot"))
 
-bot.run(TOKEN)
+bot.run(load_env.DISCORD_TOKEN)
