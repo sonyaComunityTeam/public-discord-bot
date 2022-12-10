1b6cfa09b271b7f268c907ffff5d35a8bef578f6
sonyakun <90973770+sonyakun@users.noreply.github.com>
2022-12-10 12:14:04 +0900

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,7 @@
 import discord
 import load_env
+import aiohttp
+import asyncio
 
 bot = discord.Client(intents=discord.Intents.all())
 
@@ -10,5 +12,14 @@ async def on_ready():
     print(bot.user.id)
     print('------')
     await bot.change_presence(activity=discord.Game(name="そにゃコミュニティPublic Bot"))
+
+@bot.slash_command(guild_ids=[1006776654934323260], name="expandurl", description="Expand your TinyURL")
+async def expandurl(ctx, url: str):
+    if type == "展開":
+        async with aiohttp.ClientSession() as session:
+            expand = session.get(Link).url
+            await ctx.respond(f"URLを展開しました。:{expand}")
+    elif type == "短縮":
+            await ctx.respond(f"URLを短縮しました。:{tyny}")
 
 bot.run(load_env.DISCORD_TOKEN)
